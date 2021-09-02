435506e670ea929b919e4f29434599c61ffea072
Md Arifuzzaman <md.arifuzzaman@citytech.cuny.edu>
2021-09-02 18:58:38 -0400

    Created Header and Cleaned up the Files

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+function Header() {
+    return (
+        <Nav>
+            Header
+        </Nav>
+    )
+}
+
+export default Header
+
+//CSS3 Starts From here
+
+const Nav = styled.div`
+    height: 70px;
+    background: #090b13;
+`
+
